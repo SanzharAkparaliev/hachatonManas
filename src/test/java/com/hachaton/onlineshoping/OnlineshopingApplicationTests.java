89ef6ff495de7bdaabe9463d430c12142a904655
Sanzhar Taalaibek uulu <taalaibekuulusanzhar@gmail.com>
2023-02-24 19:18:32 +0600

    first commit

---
+++ src/test/java/com/hachaton/onlineshoping/OnlineshopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hachaton.onlineshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
